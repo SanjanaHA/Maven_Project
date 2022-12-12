d5f5bf86c0e780edbe9be1cb3d56a84a3c44815e
HP <HP@Admin>
2022-12-12 18:31:36 +0530

    Updated Project

--- ELF_Maven_Training/src/test/java/Test_Case/Aprilia_Test.java
+++ ELF_Maven_Training/src/test/java/Test_Case/Aprilia_Test.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+
 public class Aprilia_Test {
 @Test
 public void app() {
